4d68d6eae0dcc30987ce2a6fb7f7c5dc892d7269
pobertraterson <pobraterson@icloud.com>
2025-03-10 08:51:43 +0000

    Not working but kinda

---
+++ src/main/java/uk/co/mediumeffortmedia/FXWorker.java
@@ -0,0 +1,28 @@
+package uk.co.mediumeffortmedia;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class FXWorker extends Application{
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("meConverter-UI_WIP"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1024, 576);
+
+        stage.setTitle("meConverter");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/uk/co/mediumeffortmedia/Main.java
+++ src/main/java/uk/co/mediumeffortmedia/Main.java
@@ -4,14 +4,9 @@ import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.*;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import java.io.IOException;
-import javafx.application.Application;
-import javafx.stage.*;
-import javafx.fxml.*;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
 
 public class Main {
-    public static void worker() throws IOException {
+    public static void main(String[] args) throws IOException {
         System.out.println("The Medium Effort Converter:meConverter");
 
         FFmpeg ffmpeg = new FFmpeg("/opt/homebrew/Cellar/ffmpeg/7.1_4/bin/ffmpeg");
@@ -35,8 +30,5 @@ public class Main {
 
         FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
         executor.createJob(builder).run();
-    }
-    public static void main(String[] args) {
-        
     }
 }
