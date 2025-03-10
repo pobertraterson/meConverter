490c5538834c40edd19417533fec809e7e4ba730
pobertraterson <pobertraterson@icloud.com>
2025-03-10 14:39:22 +0000

    Now loads blank.fxml

--- src/main/java/uk/co/mediumeffortmedia/FXWorker.java
+++ src/main/java/uk/co/mediumeffortmedia/FXWorker.java
@@ -2,7 +2,7 @@ package uk.co.mediumeffortmedia;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
@@ -12,7 +12,7 @@ public class FXWorker extends Application{
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("meConverter-UI_WIP"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("blank.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root, 1024, 576);
