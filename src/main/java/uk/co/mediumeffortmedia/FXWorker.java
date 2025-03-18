f55a4c5eb2c68572de309ac922b2d302ad600ad6
pobertraterson <pobraterson@icloud.com>
2025-03-18 17:47:35 +0000

    Button working, text box, and CSS

--- src/main/java/uk/co/mediumeffortmedia/FXWorker.java
+++ src/main/java/uk/co/mediumeffortmedia/FXWorker.java
@@ -1,18 +1,23 @@
 package uk.co.mediumeffortmedia;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.stage.*;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
 import javafx.application.Application;
 import javafx.scene.control.*;
+
+import java.io.File;
 
 public class FXWorker extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        TextField fileToConvertText = new TextField();
+        fileToConvertText.setPrefWidth(400);
+        fileToConvertText.setTranslateX(130);
+        fileToConvertText.setTranslateY(200);
+        fileToConvertText.setEditable(false);
+
 
         Button openFile = new Button();
         openFile.setText("Open File");
@@ -26,12 +31,17 @@ public class FXWorker extends Application {
                     new FileChooser.ExtensionFilter("Video Files", "*.mov","*.mp4","*.mkv","*.avi","*.wmv","*.webm"),
                     new FileChooser.ExtensionFilter("Audio Files","*.mp3","*.m4a","*.aac","*.aif","*.aiff","*.wav","*.flac","*.wma","*.ogg","*.opus")
             );
-            fileChooser.showOpenDialog(finalStage);
+            File fileToConvert = fileChooser.showOpenDialog(finalStage);
+            fileToConvertText.setText(fileToConvert.getAbsolutePath());
         });
 
+        TextField ffmpegPathTextBox = new TextField();
+        TextField ffprobePathTextBox = new TextField();
 
-        Group root = new Group(openFile);
+
+        Group root = new Group(openFile,fileToConvertText);
         Scene scene = new Scene(root,1024,576);
+        scene.getStylesheets().add("/uk/co/mediumeffortmedia/theming.css");
         scene.getStylesheets().add(String.valueOf(getClass().getResource("theming.css")));
         stage = new Stage();
         stage.setScene(scene);
