1b4d1f378ab2dd5effa0fc5416a610d6cbef5df5
pobertraterson <pobraterson@icloud.com>
2025-03-19 00:21:21 +0000

    FFMPEG Directory chooser and validation

--- src/main/java/uk/co/mediumeffortmedia/FXWorker.java
+++ src/main/java/uk/co/mediumeffortmedia/FXWorker.java
@@ -14,15 +14,15 @@ public class FXWorker extends Application {
 
         TextField fileToConvertText = new TextField();
         fileToConvertText.setPrefWidth(400);
-        fileToConvertText.setTranslateX(130);
-        fileToConvertText.setTranslateY(200);
+        fileToConvertText.setTranslateX(280);
+        fileToConvertText.setTranslateY(100);
         fileToConvertText.setEditable(false);
 
 
         Button openFile = new Button();
         openFile.setText("Open File");
-        openFile.setTranslateX(50);
-        openFile.setTranslateY(200);
+        openFile.setTranslateX(680);
+        openFile.setTranslateY(100);
         Stage finalStage = stage;
         openFile.setOnAction(actionEvent -> {
             FileChooser fileChooser = new FileChooser();
@@ -32,14 +32,43 @@ public class FXWorker extends Application {
                     new FileChooser.ExtensionFilter("Audio Files","*.mp3","*.m4a","*.aac","*.aif","*.aiff","*.wav","*.flac","*.wma","*.ogg","*.opus")
             );
             File fileToConvert = fileChooser.showOpenDialog(finalStage);
-            fileToConvertText.setText(fileToConvert.getAbsolutePath());
+            if (fileToConvert != null) {
+                fileToConvertText.setText(fileToConvert.getAbsolutePath());
+            }
         });
 
         TextField ffmpegPathTextBox = new TextField();
-        TextField ffprobePathTextBox = new TextField();
+        ffmpegPathTextBox.setPrefWidth(400);
+        ffmpegPathTextBox.setTranslateX(280);
+        ffmpegPathTextBox.setTranslateY(476);
+        ffmpegPathTextBox.setEditable(false);
 
+        Button openFFMPEG = new Button();
+        openFFMPEG.setText("Open ffmpeg");
+        openFFMPEG.setTranslateX(680);
+        openFFMPEG.setTranslateY(476);
+        openFFMPEG.setOnAction(actionEvent -> {
+            DirectoryChooser ffmpegChooser = new DirectoryChooser();
+            ffmpegChooser.setTitle("Open FFMPEG Directory");
+            File ffmpegDirectory = ffmpegChooser.showDialog(finalStage);
+            if (ffmpegDirectory != null) {
+                String ffmpegValidator;
+                File ffmpegDirectoryValidator;
+                if (System.getProperty("os.name").contains("Windows")) {
+                    ffmpegValidator = "ffmpeg.exe";
+                } else {
+                    ffmpegValidator = "ffmpeg";
+                }
+                ffmpegDirectoryValidator = new File(ffmpegDirectory, ffmpegValidator);
+                if (ffmpegDirectoryValidator.exists()) {
+                    ffmpegPathTextBox.setText(ffmpegDirectory.getAbsolutePath());
+                } else {
+                    System.out.println("ffmpeg executable not detected in directory.");
+                }
+            }
+        });
 
-        Group root = new Group(openFile,fileToConvertText);
+        Group root = new Group(openFile,fileToConvertText,openFFMPEG,ffmpegPathTextBox);
         Scene scene = new Scene(root,1024,576);
         scene.getStylesheets().add("/uk/co/mediumeffortmedia/theming.css");
         scene.getStylesheets().add(String.valueOf(getClass().getResource("theming.css")));
@@ -51,6 +80,11 @@ public class FXWorker extends Application {
     }
 
     public static void main(String[] args) {
+        String operatingSys = System.getProperty("os.name");
+        System.out.println("The Medium Effort Converter. Developed by Robert Paterson, Oliver Strange, Zaid Faisal, Kheder Tallaa, Muhammad Ibrahim, & Abdul Ibn-E-Ali");
+        System.out.println("Current Version: 1.0.0-SNAPSHOT");
+        System.out.println("System.getProperty(\"os.name\") reports:");
+        System.out.println(operatingSys);
         launch(args);
     }
 }
